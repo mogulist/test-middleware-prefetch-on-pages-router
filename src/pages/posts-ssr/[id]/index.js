f7b5a7926040f0c3bf282fca6780f130ee3d23c4
임형택/프론트엔드개발팀 <herbert@ktown4u.com>
2024-05-22 17:35:44 +0900

    Add posts with SSR

---
+++ src/pages/posts-ssr/[id]/index.js
@@ -0,0 +1,37 @@
+// pages/posts/[id].js
+
+import React from "react";
+import { useRouter } from "next/router";
+
+// Fetch data using getServerSideProps
+export async function getServerSideProps({ params }) {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.id}`
+  );
+  const post = await res.json();
+
+  return {
+    props: {
+      post,
+    },
+  };
+}
+
+const PostDetail = ({ post }) => {
+  const router = useRouter();
+
+  // If the page is not yet generated, this will be displayed initially
+  // until getServerSideProps finishes running
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="max-w-3xl mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">{post.title}</h1>
+      <p className="text-gray-700">{post.body}</p>
+    </div>
+  );
+};
+
+export default PostDetail;
---
+++ src/pages/posts-ssr/index.js
@@ -0,0 +1,40 @@
+// pages/posts.js
+
+import React from "react";
+import Link from "next/link";
+
+// Fetch data using getServerSideProps
+export async function getServerSideProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await res.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+// Component to display the list of posts
+const Posts = ({ posts }) => {
+  return (
+    <div className="max-w-3xl mx-auto p-4">
+      <h1 className="text-3xl font-bold text-center mb-6">Posts</h1>
+      <ul className="space-y-6">
+        {posts.map((post) => (
+          <li
+            key={post.id}
+            className="p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:shadow-md transition-shadow duration-300"
+          >
+            <Link href={`/posts-ssr/${post.id}`}>
+              <h2 className="text-2xl font-semibold mb-2">{post.title}</h2>
+              <p className="text-gray-700">{post.body}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Posts;
